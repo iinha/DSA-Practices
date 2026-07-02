e625fa0d55df2a325a0b33d641524590670ae28a
ainha <nnguyenvoainha@gmail.com>
2026-07-02 16:53:52 +0700

    Count the number of nodes with values ​​in the range [lo, hi].

---
+++ Tree/14. Count the number of nodes with values ​​in the range [lo, hi]/14. Count the number of nodes with values ​​in the range [lo, hi].cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void insertNode(Node*& root, Node* node) {
+    if (root == NULL) {
+        root = node;
+        return;
+    }
+
+    if (node->data < root->data) {
+        if (root->left == NULL) {
+            root->left = node;
+            return;
+        }
+        insertNode(root->left, node);
+        return;
+    }
+
+    if (root->right == NULL) {
+        root->right = node;
+        return;
+    }
+    insertNode(root->right, node);
+}
+
+int countInRange(Node* root, int lo, int hi) {
+    if (root == NULL) return 0;
+    if (root->data > hi) {
+        return countInRange(root->left, lo, hi);
+    }
+    if (root->data < lo) {
+        return countInRange(root->right, lo, hi);
+    }
+    return 1 + countInRange(root->left, lo, hi) + countInRange(root->right, lo, hi);
+}
+
+int main() {
+    Node* root = NULL;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int data;
+        cin >> data;
+        insertNode(root, createNode(data));
+    }
+
+    int lo, hi;
+    cin >> lo >> hi;
+
+    cout << countInRange(root, lo, hi) << endl;
+    return 0;
+}
