808944fae0d88ad2870264bdcd1c750c11795efb
ainha <nnguyenvoainha@gmail.com>
2026-07-04 21:59:26 +0700

    draft.

---
+++ Graph/09. Count the number of connected components with exactly k vertices/09. Count the number of connected components with exactly k vertices.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// adj la danh sach ke. Tra ve so thanh phan lien thong co dung k dinh.
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int> > adj(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int k;
+    cin >> k;
+
+    cout << countComponentsWithK(adj, n, k) << "\n";
+
+    return 0;
+}
