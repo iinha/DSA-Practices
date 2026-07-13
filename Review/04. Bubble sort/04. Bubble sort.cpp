1b74da0ada3e1d65a04a98688a64210fb04e6c55
ainha <nnguyenvoainha@gmail.com>
2026-07-13 22:25:28 +0700

    Bubble sort.

---
+++ Review/04. Bubble sort/04. Bubble sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int a[], int n) {
+    int temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    bubbleSort(a, n);
+
+    for (int i = 0; i < n; i++) {
+        if (i) cout << ' ';
+        cout << a[i];
+    }
+    cout << '\n';
+    return 0;
+}
