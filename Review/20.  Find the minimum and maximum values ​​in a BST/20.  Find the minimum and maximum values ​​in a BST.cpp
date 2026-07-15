cdd3476de7da2f692b81893134e566c9f1df4ea8
ainha <nnguyenvoainha@gmail.com>
2026-07-15 17:51:47 +0700

    Find min, max in a BST.

---
+++ Review/20.  Find the minimum and maximum values ​​in a BST/20.  Find the minimum and maximum values ​​in a BST.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void insertNode(Node*& root, Node* node) {
+    if (root == NULL) {
+        root = node;
+        return;
+    }
+    if (node->data < root->data) {
+        if (root->left == NULL) {
+            root->left = node;
+            return;
+        }
+        insertNode(root->left, node);
+        return;
+    }
+    if (root->right == NULL) {
+        root->right = node;
+        return;
+    }
+    insertNode(root->right, node);
+}
+
+int findMin(Node* root) {
+    if (root == NULL) return 0;
+    if (root->left == NULL) {
+        return root->data;
+    }
+    return findMin(root->left);
+}
+int findMax(Node* root) {
+    if (root == NULL) return 0;
+    if (root->right == NULL) {
+        return root->data;
+    }
+    return findMax(root->right);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Node* root = NULL;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int data;
+        cin >> data;
+        insertNode(root, createNode(data));
+    }
+    cout << findMin(root) << ' ' << findMax(root) << "\n";
+    return 0;
+}
