c1acf21c255adac2b2a239ac917e0d1ba10f500e
ainha <nnguyenvoainha@gmail.com>
2026-07-25 00:34:51 +0700

    Handle a customer queue using queue.

--- Review/16.  Handle a customer queue using queue/16.  Handle a customer queue using queue.cpp
+++ Review/16.  Handle a customer queue using queue/16.  Handle a customer queue using queue.cpp
@@ -10,6 +10,62 @@ using namespace std;
 //   int  dequeue()        - remove and return the front value (the queue is non-empty)
 //   int  sizeQueue()      - return the number of elements currently in the queue
 
+struct node {
+    int data;
+    node* next;
+};
+
+node* head = nullptr;
+node* tail = nullptr;
+int currentsize = 0;
+
+void enqueue(int x) {
+    node* newnode = new node();
+    newnode->data = x;
+    newnode->next = nullptr;
+
+    if (tail == nullptr) {
+        head = tail = newnode;
+    }
+    else {
+        tail->next = newnode;
+        tail = newnode;
+    }
+    currentsize++;
+}
+
+bool emptyQueue() {
+    return head == nullptr;
+}
+
+int frontValue() {
+    if (head != nullptr) {
+        return head->data;
+    }
+    return -1;
+}
+
+int dequeue() {
+    if (head != nullptr) {
+        int val = head->data;
+        node* temp = head;
+
+        head = head->next;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        delete temp;
+        currentsize--;
+        return val;
+    }
+    return -1;
+}
+
+int sizeQueue() {
+    return currentsize;
+}
+
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
